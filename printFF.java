cfcf1d2b16d7e54bc58d9e5033776afd70e512f4
Hunter Berry <hunterberry640@gmail.com>
2021-03-05 08:41:15 -0600

    almost done just need to test follow sets and format output

--- printFF.java
+++ printFF.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.StringTokenizer;
 
+
 public class printFF {
   static ArrayList<String> nonterminals = new ArrayList<>();
   static ArrayList<String> terminals = new ArrayList<>();
@@ -62,56 +63,104 @@ public class printFF {
       }
       grammar.add(currentRule);
     }
-
-    // System.out.println("\nNon-terminal symbols: " + nonterminals.toString());
-    // System.out.println("Terminal symbols: " + terminals.toString());
-    // System.out.println("Grammar Rules: " + grammar.toString());
-    System.out.println("==First Sets==");
+    for (ArrayList<String> rule : grammar) {
+      for (String string : rule) {
+        System.out.print(string + " ");
+      }
+      System.out.println();
+    }
+    System.out.println("\nNon-terminal symbols: " + nonterminals.toString());
+    System.out.println("Terminal symbols: " + terminals.toString());
+   
+    System.out.println("\nFirst Sets:\n===");
     for (String nonterm : nonterminals) {
       Set<String> set = new HashSet<>();
-      
-      set = findFirstSet(nonterm, set);
+      set = findFirstSet(nonterm, set, null);
       System.out.println(nonterm + ": " + set.toString());
     }
     
+    System.out.println("\nFollow Sets:\n===");
+    for (String nonterm : nonterminals) {
+      Set<String> set = new HashSet<>();
+      set = findFollowSet(nonterm, set, null);
+      System.out.println(nonterm + ": " + set.toString());
+    }
   }
 
-  public static Set<String> findFirstSet(String nonterminal, Set<String> set){
-    Set<String> tempSet = new HashSet<>();
+  public static Set<String> findFollowSet(String nonterminal, Set<String> set, String prev){
     for (ArrayList<String> rule : grammar) {
-      if(rule.get(0).equals(nonterminal)){
-        for (int i = 2; i < rule.size(); i++) {
-            if(terminals.contains(rule.get(i)) || rule.get(i).equals("lambda") ){ // if production is a terminal add it to first set
-              set.add(rule.get(i));
-              break;
-            }else{  
-              if(rule.get(i).equals(nonterminal) && containslambda(nonterminal)){ // if rule calls itself but contains lambda go to next symbol
-                continue;
-              }
-              else if(rule.get(i).equals(nonterminal)){ // if rule calls itself but doesn't contain lambda go to next rule
-                break;
-              }     
-              if(containslambda(rule.get(i))){
-                continue;
-              }
-              tempSet = findFirstSet(rule.get(i), set);       
-              if(tempSet.contains("lambda")){
-                // System.out.println(tempSet.toString());
-                tempSet.remove("lambda");
-                set.addAll(tempSet);
-              }else{
-                break;
+
+      for (int i = 2; i < rule.size()-1; i++){ // -1 because we will be looking ahead and don't care about last elem
+        
+      //For the start symbol S, place $ in Follow(S).
+        if(nonterminal.equals("S") || nonterminal.equals("Start")){
+          set.add("$");
+        }
+
+        if(rule.size() <=3 || !hasNonTerm(rule, nonterminal) ){ //if RHS of rule !contain nonterminal skip 
+          break;
+        }
+
+        if(rule.get(i).equals(nonterminal)){
+          if(terminals.contains(rule.get(i+1))){
+            set.add(rule.get(i+1));
+            break;
+          }else{
+           // If A → aBb, then First(b) ‐ {e} ⊆ Follow(B)
+            Set<String> temp = new HashSet<>();
+            temp = findFirstSet(rule.get(i+1), temp, rule.get(i+1));
+            
+            /*if (A → aBb and ε in First(b)),
+              then Follow(A) ⊆ Follow(B) */
+            if(temp.contains("lambda")){
+              int count = 2;
+              while(count+i <= rule.size()-1 && temp.contains("lambda")){ 
+                
+                temp = findFirstSet(rule.get(i+count), temp, rule.get(i+count));
+                
+                set.addAll(temp);
+                
+                count++;
               }
               
+            }
+            temp.remove("lambda");
+            set.addAll(temp);
+            
           }
+          
         }
+        /*if (A → aB),
+              then Follow(A) ⊆ Follow(B) */
+        if(rule.get(rule.size()-1).equals(nonterminal) && !rule.get(0).equals(nonterminal) && prev != rule.get(0)){ // if nonterm not follow by anything
+          Set<String> temp = new HashSet<>();
+          
+          temp = findFollowSet(rule.get(0), temp, nonterminal);
+          set.addAll(temp);
+          break;
+        }
+
       }
     }
+
+    
+
+    // For any production rule A → αB: Follow(B) = Follow(A)
+
+    // For any production rule A → αBβ,
+    //  If ∈ ∉ First(β), then Follow(B) = First(β)
+    //  If ∈ ∈ First(β), then Follow(B) = { First(β) – ∈ } ∪ Follow(A)
     return set;
+
   }
 
-  public static void findFollowSet(){
-    
+  public static boolean hasNonTerm(ArrayList<String> rule, String  nonterminal){
+    for (int i = 2; i < rule.size(); i++){
+      if(nonterminal.equals(rule.get(i))){
+        return true;
+      }
+    }
+    return false;
   }
 
   public static boolean containslambda(String nonterminal){
@@ -124,5 +173,44 @@ public class printFF {
     }
     return false;
   }
-  
+
+
+  public static Set<String> findFirstSet(String nonterminal, Set<String> set, String prev){
+    for (ArrayList<String> rule : grammar) {
+      if(rule.get(0).equals(nonterminal)){ // only analyze rules of current nonterminal
+        for (int i = 2; i < rule.size(); i++) {
+            if(prev != null && prev.equals(rule.get(i))){
+              break;
+            }
+            if(terminals.contains(rule.get(i)))
+            { // if A ::= t
+              set.add(rule.get(i));
+              set.remove("lambda");
+              break;
+            }else if(nonterminal.equals(rule.get(i)))
+            { // if A ::= Aa
+              if(containslambda(rule.get(i)))
+              { // if rule contains lambda go to next sym
+                continue;
+              }else
+              { // if not break out of loop because of infinite loop go to next rule
+                break;
+              }
+            }else if(containslambda(rule.get(i)))
+            { // if A ::= B where lambda in B
+              set.addAll(findFirstSet(rule.get(i), set, nonterminal));
+            }else
+            {// A ::= B where lambda not in B
+              set.remove("lambda");
+              set.addAll(findFirstSet(rule.get(i), set, nonterminal));
+              break;
+            }
+        }
+      }
+      if(containslambda(nonterminal)){ // if A ::= lambda
+        set.add("lambda");
+      }
+    }
+    return set;
+  }
 }
